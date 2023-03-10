848c9999766691f3366397a45aae8823b182c5fe
Seteve Conde da Luz <setevecondedaluz@hotmail.com>
2023-03-10 20:57:04 -0300

    new commit

--- Dominio/Models/ContatoModel.cs
+++ Dominio/Models/ContatoModel.cs
@@ -24,7 +24,7 @@ namespace Dominio.Models
         [Phone(ErrorMessage = "O celular informado não é valido!")]
         public string Celular { get; set; }
 
-        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Required(ErrorMessage = "Esta data não é valida")]
         public DateTime Data { get; set; }
     }
 }
