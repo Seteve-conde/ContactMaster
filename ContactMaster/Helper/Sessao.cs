e56f1c6b36d06f0b7f96c0618e3445b1a0b2a90f
Seteve Conde da Luz <setevecondedaluz@hotmail.com>
2023-05-04 19:17:21 -0300

    Add User session

---
+++ ContactMaster/Helper/Sessao.cs
@@ -0,0 +1,37 @@
+﻿using Dominio.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ContactMaster.Helper
+{
+    public class Sessao : ISessao
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public Sessao(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public UsuarioModel BuscarSessaoUsuario()
+        {
+            string sessaoUsuario = _httpContext.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario)) return null;
+
+            return JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+        }
+
+        public void CriarSessaoUsuario(UsuarioModel usuario)
+        {
+            string valor = JsonConvert.SerializeObject(usuario, Formatting.Indented);
+
+            _httpContext.HttpContext.Session.SetString("sessaoUsuarioLogado", valor);
+        }
+
+        public void RemoverSessaoUsuario()
+        {
+            _httpContext.HttpContext.Session.Remove("sessaoUsuarioLogado");
+        }
+    }
+}
---
+++ ContactMaster/ViewComponet/Menu.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ContactMaster.ViewComponet
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string sessaoDoUsuario =  HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoDoUsuario)) return null;
+
+            UsuarioModel usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoDoUsuario);
+
+            return View(usuario);
+        }
+    }
+}
